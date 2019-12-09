c151dd6f21a1867fba0832295d93c3c6a3ee9205
Marcus Dogramaci Svanberg <marcus_haha@live.se>
2019-12-09 12:36:41 +0100

    First commit to master
    Badrummet

---
+++ KevinLuktar/KevinLuktar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KevinLuktar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Git");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
